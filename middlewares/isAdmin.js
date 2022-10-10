413d493652b36f3d58030015ad14bffb3cd05ca2
sonu kumar <sonukumar@iitbhilai.ac.in>
2022-10-10 15:43:00 +0530

    updated user

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -5,9 +5,7 @@ const bcrypt = require("bcrypt");
 const isAdmin=async (req,res,next) =>{
     try{
         const { Adminemail, Adminpassword } = req.body;
-        // const verifyToken=jwt.verify(token,process.env.JWT_SECRET);
         const rootUser = await User.findOne({email:Adminemail});
-        console.log(Adminpassword);
         if(!rootUser || !(await bcrypt.compare( Adminpassword, rootUser.password))){throw new Error('user not found')};
 
         if(rootUser.role==0){throw new Error('admin resource! access denied!');}
